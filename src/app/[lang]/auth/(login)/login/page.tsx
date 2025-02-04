6fab04d24f85cdfec371771a0c396483643defb1
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2025-02-03 21:39:44 -0300

    Adiciona página de login e componentes de interface, remove arquivos SVG não utilizados

---
+++ src/app/[lang]/auth/(login)/login/page.tsx
@@ -0,0 +1,12 @@
+import ButtonUsage from '@/components/ButtonUsage';
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <ButtonUsage />
+    </div>
+  );
+};
+
+export default LoginPage;
